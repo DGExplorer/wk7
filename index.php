2d987993c7816ced2ef5c04bcfd3062d20cb540b
Dora <dg457@njit.edu>
2017-11-10 17:38:53 -0500

    add ind

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
+//turn on debugging messages
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
 $dsn = 'mysql:dbname=dg457;host=sql1.njit.edu';
 $user = 'dg457';
 $password = 'xx0YWWEe';
+
 try {
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -9,35 +14,12 @@ try {
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
+
+//printing the number of records where is is less than six
 $sql = "SELECT * FROM accounts WHERE id <6";
 $result = $dbh->query($sql);
-if = $dbh->query($sql)){
-    $num_row = $result->fetchColumn();
-    echo $num_row." records returned<br>";
+if ($dbh->query($sql){
+$num_row = $result->fetchColumn(); {
+    echo $num_row . " records returned<br>";}
 
-    if ($result->fetchColumn()> 0){
-        echo "<table>";
-            echo "<tr>";
-                echo "<th>id</th>";
-                echo "<th>email</th>";
-                echo "<th>fname</th>";
-                echo "<th>lname</th>";
-                echo "<th>phone</th>";
-                echo "<th>birthday</th>";
-                echo "<th>gender</th>";
-                echo "<th>password</th>";
-                
-
-
-
-    }
-
-}
-try {
-
-    $dbh->exec($sql);
-} catch (PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
-}
-echo 'done';
 ?>
